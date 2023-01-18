543b103e6f3383216297e3fac8712352f4870b3e
kiran madiwalar <kiranm81040@gmail.com>
2023-01-18 11:41:48 +0530

    Q1 added

---
+++ JAVASCRIPT ASSIGNMENT/Q1/variable.js
@@ -0,0 +1,10 @@
+let x = "kiran";
+let Social = true;
+let hello=undefined;
+let money = null;
+
+console.log( " value:"+ x +"  datatype:"+ typeof x);
+console.log(" value:"+ Social + "   datatype:"+ typeof Social);
+console.log(" value:"+ hello + "   datatype:"+typeof hello);
+console.log(" value:"+ money + "   datatype:" + typeof money);
+
