f3f56079d1241b07300aa98dd69ca7f55ac1a73d
kiran madiwalar <kiranm81040@gmail.com>
2023-01-18 11:59:27 +0530

    Q2 added

---
+++ JAVASCRIPT ASSIGNMENT/Q2/bio.js
@@ -0,0 +1,7 @@
+let firstName = "kiran"
+let lastName = "madiwalar"
+let martialStatus = "unmarrid"
+let age = 21
+let country = "India"
+  
+console.log(`I am ${firstName}` +` ${lastName}`+ ` I am ${martialStatus}`+ ` at the age of ${age}` +` and I am from ${country}`);
