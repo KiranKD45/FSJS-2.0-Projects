058e2bd6bfbd7b6646ffead3a9739ddc44fd1e66
kiran madiwalar <kiranm81040@gmail.com>
2023-01-25 11:59:34 +0530

    practice updated

---
+++ XYZ PRACTICE 2.0/css and html practice/script.js
@@ -0,0 +1,76 @@
+// // const obejct={
+// //     rocket:'1',
+// //     kd:'2'
+// // }
+// // console.log(obejct.kd);
+
+
+// // const obj1={
+// //     name:'kd',
+// //     age:21,
+// //     config:{
+// //         name:'moon'
+// //     }
+// // }
+// // obj1.age=25;
+// // console.log(obj1.age);
+
+// // const obj2=new Object()
+// // obj2.name='kiran'
+// // obj2.age=21
+// // console.log(obj2.age);
+
+// // const powers={
+// //     name:'kd',
+// //     cordinate: Math.random()+2
+// // }
+// // // const obj3 = Object.create(powers)
+// // // console.log(Object.getPrototypeOf(obj3));
+ 
+// // const obj4 = Object.create({})
+// // Object.defineProperty(obj4, 'name', {
+// //     get : ()=> 'hi',
+// //     enumerable: true
+// // })
+// // console.log(obj4.name);
+
+// let myHeros = ["thor", "spiderman"]
+// let dcHeros = ["batman", "black adam", "superman"]
+
+
+// let heropower = {
+//     thor: "hammer",
+//     spiderman: "sling",
+
+//     getSpiderPower: function(){
+//         console.log(`Spidy power is ${this.spiderman}`);
+//     }
+// }
+
+// Object.prototype.hitesh = function(){
+//     console.log(`Hitesh is peresent in all objects`);
+// }
+
+// Array.prototype.heyHitesh = function(){
+//     console.log(`Hitesh says hello`);
+// }
+
+// // myHeros.hitesh()
+// let companyName = "GOOGLE";
+
+// let companyNameInArr = companyName.split("");
+
+// console.log(companyNameInArr);
+// let kiran = "heyyy";
+// let kd = true;
+// let say = undefined;
+// let score = null
+// console.log(kiran + kd + typeof score);
+
+let firstName = "Kiran"
+let lastName = " Madiwalar"
+let martialStatus = " no"
+let country = " India"
+let age = 22
+
+console.log(firstName + lastName + martialStatus + country + age);
