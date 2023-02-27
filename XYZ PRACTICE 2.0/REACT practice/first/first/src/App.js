db479cbff7c99287467f3fba946e221526ff0bcf
kiran madiwalar <kiranm81040@gmail.com>
2023-02-27 19:37:26 +0530

    react hello world added

--- XYZ PRACTICE 2.0/REACT practice/first/first/src/App.js
+++ XYZ PRACTICE 2.0/REACT practice/first/first/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         hello boysss
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function app() {
+    return (
+        <>
+        <h1> hello testinggggg!!!!
+            finallyy
+        </h1>
+        <h2>its me kiran </h2>
+        </>
+    );
+    
 }
-
-export default App;
+export default app;
