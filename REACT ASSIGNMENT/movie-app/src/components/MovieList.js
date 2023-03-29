892ab44a909079ee45f6be79d3d44789ab5b93c9
kiran madiwalar <kiranm81040@gmail.com>
2023-03-29 18:52:40 +0530

    Movie app completed

--- REACT ASSIGNMENT/movie-app/src/components/MovieList.js
+++ REACT ASSIGNMENT/movie-app/src/components/MovieList.js
@@ -6,8 +6,16 @@ const MovieList = (props) => {
 	return (
 		<>
 			{props.movies.map((movie, index) => (
+
 				<div className='image-container d-flex justify-content-start m-3'>
 					<img src={movie.Poster} alt='movie'></img>
+					<li className='list-content'>
+					<h3 className='content'>Title:{movie.Title}</h3>
+					<h3 className='content'>Year:{movie.Year}</h3>
+					<h3 className='content'>Type:{movie.Type}</h3>
+					<h3 className='content'>imdbID:{movie.imdbID}</h3>
+					</li>
+					
 					<div
 						onClick={() => props.handleFavouritesClick(movie)}
 						className='overlay d-flex align-items-center justify-content-center'
